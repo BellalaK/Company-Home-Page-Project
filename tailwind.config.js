48aa542fb0e115beb86b17682a2e4d7da4af2a76
BellalaK <bellafreesh@gmail.com>
2025-01-12 14:55:47 -0600

    Refactor HTML structure and styles for improved layout and responsiveness; update Tailwind CSS configuration for color consistency.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,16 +11,16 @@ module.exports = {
     colors: {
       transparent: "transparent",
       rose: {
-        10: #fff7f9,
-        20: #ffdce5,
-        30: #ff3b8d,
-        40: #db0072,
-        50: #800040,
-        60: #4c0023
+        10: "#fff7f9",
+        20: "#ffdce5",
+        30: "#ff3b8d",
+        40: "#db0072",
+        50: "#800040",
+        60: "#4c0023",
         },
       white: "#ffffff",
       black: "#000000",
-    }
+    },
     extend: {},
   },
   plugins: [],
